318fbb71a8b3359979b127c2ee40a3952642dfe1
Jimmy Lin <yfenglin@amazon.com>
2022-07-16 06:58:52 -0400

    pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -2,7 +2,7 @@ import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import {
   CodePipeline,
   CodePipelineSource,
-  ShellStep,
+  CodeBuildStep,
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 import { PipelineStage } from "./pipeline-stage";
@@ -13,28 +13,32 @@ export class PipelineStack extends Stack {
 
     // Pipelines declaration
     const pipelineProd = new CodePipeline(this, "PipelineProd", {
-      pipelineName: "IacPipelineProd",
-      synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub("yjimmyl/cdk-app", "main"),
+      pipelineName: "PipelineProd",
+      synth: new CodeBuildStep("Synth", {
+        input: CodePipelineSource.connection("yjimmyl/cdk-app", "main", {
+          connectionArn: "arn:aws:codestar-connections:us-east-1:051075623756:connection/77a15bc1-f10e-4f80-98cd-9acbed5d781f"
+        }),
         installCommands: ["npm install -g aws-cdk"],
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
 
-    
     const pipelineDev = new CodePipeline(this, "PipelineDev", {
-      pipelineName: "IacPipelineDev",
-      synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub("yjimmyl/cdk-app", "dev"),
+      pipelineName: "PipelineDev",
+      synth: new CodeBuildStep("Synth", {
+        input: CodePipelineSource.connection("yjimmyl/cdk-app", "dev", {
+          connectionArn: "arn:aws:codestar-connections:us-east-1:051075623756:connection/77a15bc1-f10e-4f80-98cd-9acbed5d781f"
+        }),
         installCommands: ["npm install -g aws-cdk"],
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
 
     // Add deployment stage to pipelines
-    const deployProd = new PipelineStage(this, "Deploy-prod");
+    const deployProd = new PipelineStage(this, "Deploy-Prod");
     const deployStageProd = pipelineProd.addStage(deployProd);
-    const deployDev = new PipelineStage(this, "Deploy-dev");
+    
+    const deployDev = new PipelineStage(this, "Deploy-Dev");
     const deployStageDev = pipelineDev.addStage(deployDev);
 
     /*
--- test/cdk-iac-app.test.ts
+++ test/cdk-iac-app.test.ts
@@ -1,11 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as CdkIacApp from '../lib/cdk-iac-app-stack';
+import * as CdkApp from '../lib/cdk-app-stack';
 
 test('SQS Queue and SNS Topic Created', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new CdkIacApp.CdkIacAppStack(app, 'MyTestStack');
+  const stack = new CdkApp.CdkAppStack(app, 'MyTestStack');
   // THEN
 
   const template = Template.fromStack(stack);
