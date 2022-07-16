49f94aec24dfb96b21348269527daf6eb188b21e
Jimmy Lin <yfenglin@amazon.com>
2022-07-16 07:00:22 -0400

    renamed lambda

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -7,7 +7,7 @@ export class CdkAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const addAccount = new lambda.Function(this, "TestLambdaFunc", {
+    const testLambda = new lambda.Function(this, "TestLambdaFunc", {
       runtime: lambda.Runtime.NODEJS_16_X,
       code: lambda.Code.fromAsset("lambda"),
       handler: "testlam.handler",
